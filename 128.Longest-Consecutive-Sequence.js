329f805bd8cac7317f76cf221d86ca3b24fe406c
Debasish Pothal <debasish_pothal@hotmail.com>
2025-07-31 18:20:27 +0530

    Update 128.Longest-Consecutive-Sequence.js

--- 128.Longest-Consecutive-Sequence.js
+++ 128.Longest-Consecutive-Sequence.js
@@ -4,40 +4,20 @@
  */
 var longestConsecutive = function (nums) {
   const set = new Set(nums);
-
-  const map = new Map();
-
-  const hasLeftNeighbour = (num) => {
-    if (set.has(num - 1)) {
-      return true;
-    }
-
-    return false;
-  };
-
-  const isStart = (num) => {
-    return !hasLeftNeighbour(num);
-  };
-
-  const popolateNextNeighbour = (num) => {
-    let i = num + 1;
-    while (set.has(i)) {
-      map.set(num, [...map.get(num), i]);
-      i++;
-    }
-  };
-
-  for (const num of nums) {
-    if (isStart(num)) {
-      map.set(num, [num]);
-      popolateNextNeighbour(num);
-    }
-  }
-
   let maxLength = 0;
 
-  for (const value of map.values()) {
-    maxLength = Math.max(maxLength, value.length);
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentLength = 1;
+
+      while (set.has(currentNum + 1)) {
+        currentNum += 1;
+        currentLength += 1;
+      }
+
+      maxLength = Math.max(maxLength, currentLength);
+    }
   }
 
   return maxLength;
