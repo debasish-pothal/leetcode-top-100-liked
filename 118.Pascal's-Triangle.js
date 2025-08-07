e07bc3b8928586af0d2452d9e3935fe043a8bb8a
Debasish Pothal <debasish_pothal@hotmail.com>
2025-08-07 19:09:27 +0530

    Create 118.Pascal's-Triangle.js

---
+++ 118.Pascal's-Triangle.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  const result = [];
+
+  for (let i = 0; i < numRows; i++) {
+    result[i] = [];
+
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        result[i][j] = 1;
+      } else {
+        result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+      }
+    }
+  }
+
+  return result;
+};
