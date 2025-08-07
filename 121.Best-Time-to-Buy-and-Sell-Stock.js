be60ae2b278faee8ee69220be9a5ce98b9994ddb
Debasish Pothal <debasish_pothal@hotmail.com>
2025-08-07 19:49:47 +0530

    Create 121.Best-Time-to-Buy-and-Sell-Stock.js

---
+++ 121.Best-Time-to-Buy-and-Sell-Stock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let profit = 0;
+  let buyPrice = prices[0];
+
+  for (let i = 1; i < prices.length; i++) {
+    buyPrice = Math.min(buyPrice, prices[i]);
+    profit = Math.max(profit, prices[i] - buyPrice);
+  }
+
+  return profit;
+};
