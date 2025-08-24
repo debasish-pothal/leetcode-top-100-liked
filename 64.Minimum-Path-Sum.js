94783c21592db051613199a7e63c7e8d6cf2313e
Debasish Pothal <debasish_pothal@hotmail.com>
2025-08-24 06:06:53 +0530

    Update 64.Minimum-Path-Sum.js

--- 64.Minimum-Path-Sum.js
+++ 64.Minimum-Path-Sum.js
@@ -6,16 +6,14 @@ var minPathSum = function (grid) {
   const rows = grid.length;
   const cols = grid[0].length;
 
-  const result = Array.from({ length: rows + 1 }, () =>
-    Array(cols + 1).fill(Infinity)
-  );
-  result[rows][cols - 1] = 0;
+  const result = new Array(cols + 1).fill(Infinity);
+  result[cols - 1] = 0;
 
   for (let r = rows - 1; r >= 0; r--) {
     for (let c = cols - 1; c >= 0; c--) {
-      result[r][c] = grid[r][c] + Math.min(result[r + 1][c], result[r][c + 1]);
+      result[c] = grid[r][c] + Math.min(result[c], result[c + 1]);
     }
   }
 
-  return result[0][0];
+  return result[0];
 };
