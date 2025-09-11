31961a5561c92c127cbd9cc97575e67d64b071c5
Debasish Pothal <debasish_pothal@hotmail.com>
2025-09-11 19:38:55 +0530

    Create 124.Binary-Tree-Maximum-Path-Sum.js

---
+++ 124.Binary-Tree-Maximum-Path-Sum.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function (root) {
+  let result = root.val;
+
+  const dfs = (node) => {
+    if (!node) {
+      return 0;
+    }
+
+    const leftMax = Math.max(dfs(node.left), 0);
+    const rightMax = Math.max(dfs(node.right), 0);
+
+    result = Math.max(result, node.val + leftMax + rightMax);
+
+    return node.val + Math.max(leftMax, rightMax);
+  };
+
+  dfs(root);
+
+  return result;
+};
