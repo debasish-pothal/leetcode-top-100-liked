8a3ee09d412f8cc9ca2f58bc34ebc62069e6ee46
Debasish Pothal <debasish_pothal@hotmail.com>
2025-09-15 19:41:39 +0530

    Create 76.Minimum-Window-Substring.js

---
+++ 76.Minimum-Window-Substring.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function (s, t) {
+  if (s.length < t.length) return "";
+
+  const need = new Map();
+
+  for (const c of t) {
+    need.set(c, (need.get(c) || 0) + 1);
+  }
+
+  let left = 0;
+  let right = 0;
+  let valid = 0;
+  const window = new Map();
+  let start = 0;
+  let len = Infinity;
+
+  while (right < s.length) {
+    const c = s[right];
+    right++;
+
+    if (need.has(c)) {
+      window.set(c, (window.get(c) || 0) + 1);
+      if (window.get(c) === need.get(c)) {
+        valid++;
+      }
+    }
+
+    while (valid === need.size) {
+      if (right - left < len) {
+        start = left;
+        len = right - left;
+      }
+
+      const d = s[left];
+      left++;
+
+      if (need.has(d)) {
+        if (window.get(d) === need.get(d)) {
+          valid--;
+        }
+        window.set(d, window.get(d) - 1);
+      }
+    }
+  }
+
+  return len === Infinity ? "" : s.substring(start, start + len);
+};
